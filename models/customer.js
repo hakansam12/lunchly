c6a7bbc930a26e2154a18575e108dbe578dac43c
Hakan Sam <sammy@Hakans-MacBook-Air.local>
2020-11-30 09:40:01 -0500

    added fullName property and added save rezervation property

--- models/customer.js
+++ models/customer.js
@@ -53,6 +53,12 @@ class Customer {
     return new Customer(customer);
   }
 
+  // property to get the full name
+
+  get fullName(){
+    return `${this.firstName} ${this.lastName}`;
+  }
+
   /** get all reservations for this customer. */
 
   async getReservations() {
